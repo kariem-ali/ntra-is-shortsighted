a47bd65f1c3a0b663fee4e57bb7b42d0b237e329
Kariem Ali <kariem.elkoush@gmail.com>
2011-10-01 19:08:48 +0000

    Added basic eligible contacts display functionality and basic UI
    git-svn-id: https://ntra-is-shortsighted.googlecode.com/svn/trunk@4 a0271c76-e70c-cabd-fc6e-87bfdf9e2f20

---
+++ js/Contact.js
@@ -0,0 +1,23 @@
+﻿if (typeof BB === 'undefined')
+  BB = {};
+
+BB._contacts = {};
+
+BB._allPhones = [
+  'faxPhone', 'homePhone', 'homePhone2', 'mobilePhone',
+  'otherPhone', 'pagerPhone', 'workPhone', 'workPhone2'
+];
+
+BB.Contact = {
+  init: function (contact) {
+    this._contact = contact;
+    this.name = contact.firstName + ' ' + contact.lastName;
+    this.phones = {};
+
+    for (var i = 0; i < BB._allPhones.length; i++) {
+      var currentPhone = BB._allPhones[i];
+      if (typeof contact[currentPhone] !== 'undefined')
+        this.phones[currentPhone] = contact[BB._allPhones[i]];
+    }
+  }
+};
--- js/app.js
+++ js/app.js
@@ -1,1 +1,54 @@
-﻿
+﻿if (typeof Object.create !== 'function') {
+  Object.create = function (o) {
+    function f() { }
+    f.prototype = o;
+    return new f();
+  };
+}
+
+function loadContacts() {
+  var contacts = getEligibleContacts('^([\+0]0*2)*(01)([0-246-9]{1}|5[0-25])([0-9]{7})$');
+  buildUI(contacts);
+}
+
+function getEligibleContacts(regExp) {
+  var contacts = [];
+  
+  for (var k = 0; k < BB._allPhones.length; k++) {
+    var filter = new blackberry.find.FilterExpression(BB._allPhones[k], 'REGEX', regExp);
+    contacts = contacts.concat(blackberry.pim.Contact.find(filter));
+  }
+  return contacts;
+}
+
+function initContact(bbContact) {
+  var contact = Object.create(BB.Contact);
+  contact.init(bbContact);
+  BB._contacts[contact.name] = contact;
+  return contact;
+}
+
+function buildUI(contacts) {
+  var contactsDiv = document.getElementById('contacts');
+
+  for (var i = 0; i < contacts.length; i++) {
+    var contact = initContact(contacts[i]);
+
+    var container = document.createElement('div');
+    container.className += ' container';
+
+    var nameDiv = document.createElement('div');
+    nameDiv.className += ' name';
+    nameDiv.appendChild(document.createTextNode(contact.name));
+    
+    container.appendChild(nameDiv);
+
+    for (var phone in contact.phones) {
+      var phoneDiv = document.createElement('div');
+      phoneDiv.className += ' phone';
+      phoneDiv.appendChild(document.createTextNode(contact.phones[phone]));
+      container.appendChild(phoneDiv);
+    }
+    contactsDiv.appendChild(container);
+  }  
+}
